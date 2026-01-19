634a3679beff73889ebb425ee6be91846c6f5d14
Umar Olowonyo <olowonyoumar@gmail.com>
2026-01-19 18:58:46 +0100

    Completed the constant signup form feature. If a user is filling the signup form and reloads the page, all the info that they've typed will still be there

--- website/static/js/control_signup.js
+++ website/static/js/control_signup.js
@@ -1,7 +1,12 @@
 
+/*
+    I want to constantly check for when the user checks the checkbox.
+    When they check the check box, save the items that they have typed to local storage.
+    When ever the page is reloaded, take the items stored in local storage and place them in the input fields.
+*/
+
 
 const sign_up_btn_submit = document.getElementById("sign_up_btn_submit")
-
 
 const sign_up_form = document.getElementById("SignUpForm")
 
@@ -9,14 +14,11 @@ const sign_up_information = document.querySelectorAll(".form-control") // This g
 
 const necessary_signup_information = Array.from(sign_up_information).slice(0, 5) //Here I get all necessary user information
 
-const checkbox_confirm = document.getElementById("confirm_inputs")
 
 let info_useful_object = {
                     first_name: "",
                     last_name:  "",
                     email_signup: "",
-                    password1_signup: "",
-                    password2_signup: "",
 };
 
 let info_as_strings;
@@ -24,58 +26,85 @@ let info_as_strings;
 const first_name_input = necessary_signup_information[0]
 const last_name_input = necessary_signup_information[1]
 const email_signup_input = necessary_signup_information[2]
-const password1_signup_input = necessary_signup_information[3]
-const password2_signup_input = necessary_signup_information[4]
 
 
-document.addEventListener("DOMContentLoaded", showInfo)
+function check_info(){
 
-function check_checkbox(){
+    const isFirstNameValid = first_name_input.value.trim().length >= 2;
+    const isLastNameValid = last_name_input.value.trim().length >= 2;
+    const isEmailValid = email_signup_input.value.includes(".") && email_signup_input.value.includes("@") && email_signup_input.value.length >= 5;
 
-    if(checkbox_confirm.checked){
+
+    let first_name = first_name_input.value
+    let last_name = last_name_input.value
+    let email_signup = email_signup_input.value
+                
+    info_useful_object.first_name = first_name
+    info_useful_object.last_name = last_name
+    info_useful_object.email_signup = email_signup
+                  
+    info_as_strings = JSON.stringify(info_useful_object)
+    sessionStorage.setItem("user_information_key", info_as_strings)
+        
 
 
-        let first_name = first_name_input.value
-        let last_name = last_name_input.value
-        let email_signup = email_signup_input.value
-        let password1_signup = password1_signup_input.value
-        let password2_signup = password2_signup_input.value
-    
-        info_useful_object.first_name = first_name
-        info_useful_object.last_name = last_name
-        info_useful_object.email_signup = email_signup
-        info_useful_object.password1_signup = password1_signup
-        info_useful_object.password2_signup = password2_signup
-    
-    
-        info_as_strings = JSON.stringify(info_useful_object)
-        localStorage.setItem(storageKey, info_as_strings)
-    
-        sign_up_btn_submit.disabled = "false"
-    
+    if(isFirstNameValid && isLastNameValid && isEmailValid){
+        sign_up_btn_submit.disabled = false
     }
-    
+    else{
+        sign_up_btn_submit.disabled = true
+    }
+
 
 }
 
 
 function showInfo(){
 
-    const oldinfo = localStorage.getItem(storageKey)
+    const invalidValues = new Set(["", " ", null, undefined]);
 
-    let first_name = oldinfo.first_name
-    let last_name = oldinfo.last_name
-    let email_signup = oldinfo.email_signup
-    let password1_signup = oldinfo.password1_signup
-    let password2_signup = oldinfo.password2_signup
+    const oldinfo = sessionStorage.getItem("user_information_key")
 
-    first_name_input.value = first_name
-    last_name_input.value = last_name
-    email_signup_input.value = email_signup
-    password1_signup_input.value = password1_signup
-    password2_signup_input.value = password2_signup
+    if(!(invalidValues.has(oldinfo))){ //This checks to make sure that _oldinfo_ is valid. To make sure that it is 'defined'
 
+        const oldinfo_object = JSON.parse(oldinfo)
+
+        const oldinfo_array = Object.entries(oldinfo_object)
+    
+        for(let [key_object, value_object] of oldinfo_array){ //This goes through every key, value and checks them
+            if(!(invalidValues.has(value_object.trim()))){ //This makes sure that the _value_ is a valid variable
+
+                switch(true){
+                    case key_object == "first_name":
+                        first_name_input.value = value_object
+                        break
+
+                    case key_object == "last_name":
+                        last_name_input.value = value_object
+                        break
+
+                    case key_object == "email_signup":
+                        email_signup_input.value = value_object
+                        break
+
+                }
+    
+            }
+        }
+
+    }
+    
 }
 
 
+document.addEventListener("DOMContentLoaded", (event) => {showInfo()})
 
+
+sign_up_form.addEventListener("input", (event) => {check_info()}); // Triggers whenever a user types
+
+
+sign_up_form.addEventListener("submit", (event) => {
+    sessionStorage.removeItem("user_information_key");
+
+    info_useful_object = { first_name: "", last_name: "", email_signup: "" };
+});
