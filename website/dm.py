3d6bc9a176c4fe8ff6484784a4d9266f35b8a144
Umar <olowonyoumar@gmail.com>
2026-04-27 19:47:05 +0100

    Fixed all bugs with the DM feature.

--- website/dm.py
+++ website/dm.py
@@ -4,8 +4,6 @@ from flask_login import login_user, logout_user, current_user, login_required
 import random
 import string
 from .models import User, Task, Post, Message
-import os
-import uuid
 from . import db, socketio
 import datetime
 from sqlalchemy import or_
@@ -21,6 +19,7 @@ rooms = {} #This is a dictionary that will store the room codes and the users in
 def send_dm(userName):
 
     recepient = User.query.filter_by(userName=userName).first_or_404()
+
 
     session["recepient_id"] = recepient.id #We store the recepient_id in the session so that we can access it in the socketio event handlers. This way, when a message is sent, we can know who the recepient of the message is and we can save the message to the database with the correct sender and recepient ids.
 
@@ -47,37 +46,12 @@ def send_dm(userName):
     return render_template("dm.html", recepient = recepient, room_id = room_id, messages = messages)
 
 
-@socketio.on("join")
-def connect(data):
-
-    room_id = data.get("room_id")
-
-    if room_id in rooms:
-        join_room(room_id)
-        session["room_id"] = room_id
-
-        print(f"\n'{current_user.userName}' has joined '{room_id}'\n")
-
-        send(f"'{current_user.userName}' has entered the room.", to=room_id)   
-
-
-    else:
-        leave_room(room_id)
-
-
-@socketio.on("disconnect")
-def disconnect():
-
-    room_id = session.get("room_id")
-
-    print(f"\n'{current_user.userName}' has left room '{room_id}'.\n")
-
-
-@socketio.on("send_message")
+@socketio.on("send_message_to_human")
 def send_message(data):
 
     room_id = data.get("room_id")
-    msg_content = data.get("msg_content")                
+    msg_content = data.get("msg_content")    
+    recepient_id = data.get("recepient_id")   
 
     if not room_id or not msg_content: #This checks if the room_id or msg_content is empty. If either of them is empty, we don't want to send the message and we can just return from the function.
         return
@@ -90,13 +64,9 @@ def send_message(data):
         "timestamp": datetime.datetime.now().strftime('%H:%M') 
     }
 
-    print(f"\nReceived message from '{current_user.userName}' in '{room_id}': '{msg_content}'\n")
+    emit("receive_message_human", reply_data)
 
-    print(f"User {current_user.userName} is {'authenticated' if current_user.is_authenticated else 'not authenticated'}.\n")
-
-    emit("receive_message", reply_data, to=room_id)
-
-    message = Message(senderID=current_user.id, recepientID=session.get("recepient_id"), content=msg_content, timeStamp=datetime.datetime.now())
+    message = Message(senderID=current_user.id, recepientID=recepient_id, content=msg_content, timeStamp=datetime.datetime.now())
 
     db.session.add(message)
     db.session.commit()    
