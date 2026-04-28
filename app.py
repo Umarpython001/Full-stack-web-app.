44808a30ee7bc9279fd30cad6fa9fd9f93e139f9
Umar <olowonyoumar@gmail.com>
2026-04-29 00:37:42 +0100

    Used env variable to control debug mode

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from website import create_app, socketio
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     app = create_app()
     socketio.init_app(app)
-    socketio.run(app, debug=True, host="0.0.0.0")
+
+    is_debug = os.getenv("FLASK_DEBUG", "0") == "1"
+
+    socketio.run(app, debug=is_debug, host="0.0.0.0")
